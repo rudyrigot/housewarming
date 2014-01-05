1f89c6b642aa31bb6659e6e52d535bf70c2ca9bf
Rudy Rigot <rudy.rigot@gmail.com>
2014-01-04 16:03:07 -0800

    ScratchPad

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var wasScratched = false;
 
+$("#secretcode").wScratchPad({
+    width: 200,
+    height: 100,
+    image: "img/secretcode.png",
+    color: '#a5a5a5',
+    size: 20
+})
+$('#secretcode-wrapper').mouseover(
+	function() {
+    	if (!wasScratched) {
+    		$('#scratch').hide();
+    		wasScratched = true;
+    	}
+    }
+);
